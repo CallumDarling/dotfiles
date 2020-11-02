bcf117dea5b863893856caf96c47b6560f98b423
Callum Darling <callum.darling1@gmail.com>
2020-11-02 17:42:37 +0000

    emacks

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,16 +31,20 @@
   "Take a screenshot into a time stamped unique-named file in the
 same directory as the org-buffer and insert a link to this file."
   (interactive)
+  (if (not (file-exists-p "images"))
+      (make-directory "images")
+      (message "Directory Exists"))
   (setq filename
         (concat
          (make-temp-name
-          (concat (buffer-file-name)
+          (concat (file-name-directory (buffer-file-name))
+		  "images/"
+		  (file-name-base (buffer-file-name))
                   "_"
                   (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
   (call-process "import" nil nil nil filename)
   (insert (concat "[[" filename "]]"))
   (org-display-inline-images))
-
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
